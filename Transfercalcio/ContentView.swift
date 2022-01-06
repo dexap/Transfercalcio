7bb5c7350c1830251897909066943a1f1c5faa1a
dexap <bnjmnlssmnn@gmail.com>
2022-01-06 11:39:26 +0100

    Initial Commit

---
+++ Transfercalcio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Transfercalcio
+//
+//  Created by Benjamin Lassmann on 06.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Transfercalcio/TransfercalcioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransfercalcioApp.swift
+//  Transfercalcio
+//
+//  Created by Benjamin Lassmann on 06.01.22.
+//
+
+import SwiftUI
+
+@main
+struct TransfercalcioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
